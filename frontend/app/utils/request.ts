6905157bc0ae5244f06fefb66f2f87da85ac767f
MiaoMint <44718819+MiaoMint@users.noreply.github.com>
2024-12-20 20:23:31 +0800

    Update API base URL to use environment variable for improved configuration

--- frontend/app/utils/request.ts
+++ frontend/app/utils/request.ts
@@ -4,7 +4,7 @@ import { toast } from "@/hooks/use-toast";
 
 // Create base instance
 const request = ofetch.create({
-  baseURL: "http://127.0.0.1:8000/", // Your base API URL
+  baseURL: process.env.API_BASE_URL, // Your base API URL
   // Add auth token to requests
   async onRequest({ request, options }) {
     const token = tokenStorage.get();
